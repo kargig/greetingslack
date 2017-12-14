60a0b39578e0184c718eeb899c3463933338effc
George Kargiotakis <kargig@void.gr>
2017-12-14 16:03:45 +0200

    Add quotes support
    Add the ability to store and retrieve quotes in an sqlite3 database.
    Utilize FTS5[1] to provide Full Text Search capabilities.
    
    Addition of quotes happens via '!add' command.
    Retrieval of quotes happens via '!quote' command. If one gives an
    argument after the quote command, e.g. '!quote keyword' then an FTS for
    the 'keyword' happens. Adding more keywords, e.g. '!quote keyword1
    keyword2' makes the search match both keywords and returns the row
    containing both keywords. Prefix queries are also supported, e.g.
    '!quote keyword1 keyw*'
    
    [1] https://sqlite.org/fts5.html

--- bot.py
+++ bot.py
@@ -8,6 +8,9 @@ import requests
 import urllib
 import os
 import sys
+import sqlite3
+import time
+import re
 
 logging.basicConfig(filename='bot.log', level=logging.DEBUG,
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -35,6 +38,11 @@ try:
 except:
         DEBUG_CHANNEL_ID = 'Manually set the Channel if youre not running through heroku or have not set vars in ENV'
 
+try:
+        DB_FILE = os.environ['DB_FILE']
+except:
+        DB_FILE = 'FALSE'
+
 ###############################################################
 
 def welcome_message():
@@ -60,18 +68,37 @@ def welcome_me(msg):
     else:
         return False
 
-def parse_join(message):
+def is_message(msg):
+    if msg['type'] == 'message':
+        return True
+    else:
+        return False
+
+
+def show_quote(msg):
+    if msg['type'] == 'message' and msg['text'] == '!quote':
+        return True
+    else:
+        return False
+
+def add_quote(msg):
+    if msg['type'] == 'message' and msg['text'] == '!add':
+        return True
+    else:
+        return False
+
+def parse_message(message):
     m = json.loads(message)
     if is_team_join(m) or is_debug_channel_join(m) or welcome_me(m):
         user_id = m["user"]["id"] if is_team_join(m) else m["user"]
-        logging.debug(m)
+        #logging.debug(m)
         getdata= {
                 'token': TOKEN,
                 'user': user_id
                 }
         userdata = requests.get("https://slack.com/api/im.open", params=getdata)
         userdata = userdata.json()
-        logging.debug(userdata)
+        #logging.debug(userdata)
         dmchannel = userdata["channel"]["id"]
         data = {
                 'token': TOKEN,
@@ -82,22 +109,62 @@ def parse_join(message):
         }
         if (UNFURL.lower() == "false"):
           data['unfurl_link']='false'
-        logging.debug(data)
+        #logging.debug(data)
         send_message = requests.post("https://slack.com/api/chat.postMessage", data=data)
-        logging.debug(send_message)
+        #logging.debug(send_message)
         logging.debug('HELLO SENT: ' + user_id)
+    elif is_message(m):
+        ret = None
+        #logging.debug(m)
+        if m['text'][0] != '!':
+            return
+        try:
+            cmd, args = m['text'].split(' ', 1)
+        except ValueError:
+            cmd = m['text']
+            args = ''
+        udata= {
+                'token': TOKEN,
+                'user': m['user']
+                }
+        userdata = requests.get("https://slack.com/api/users.info", params=udata)
+        userdata = userdata.json()
+        logging.debug(userdata)
+        cdata= {
+                'token': TOKEN,
+                'channel': m['channel']
+                }
+        channeldata = requests.get("https://slack.com/api/channels.info", params=cdata)
+        channeldata = channeldata.json()
+        logging.debug(channeldata)
+        f_args = [userdata['user']['profile']['display_name'], channeldata['channel']['name'], m['text']]
+        if cmd == '!quote':
+            ret = quote_api().get_quote(*f_args)
+        elif cmd == '!add':
+            ret = quote_api().addtodb(*f_args)
 
+        if ret:
+            ret = ret.replace('@', '')
+            data = {
+                    'token': TOKEN,
+                    'channel': m['channel'],
+                    'text': ret,
+                    'parse': 'full',
+                    'as_user': 'true',
+            }
+            send_message = requests.post("https://slack.com/api/chat.postMessage", data=data)
+            logging.debug(send_message)
 
 #Connects to Slacks and initiates socket handshake
 def start_rtm():
     r = requests.get("https://slack.com/api/rtm.start?token="+TOKEN)
     r = r.json()
-    logging.debug(r)
+    #logging.debug(r)
     r = r["url"]
     return r
 
 def on_message(ws, message):
-    parse_join(message)
+    parse_message(message)
 
 def on_error(ws, error):
     try:
@@ -117,8 +184,130 @@ def on_close(ws):
 def on_open(ws):
     logging.info("Connection Started - Auto Greeting new joiners to the network")
 
+class quote_api:
+
+    def __init__(self):
+        """
+        """
+
+
+    def get_quote(self, user, channel, msg):
+        toks = msg.split(' ')
+        dba = db_api()
+        final_text = None
+        quote_text = None
+        out = None
+        if len(toks) < 2:
+            query = '''SELECT id, quote,added_by,quote_dt FROM quotes,quotes_fts where quotes.rowid=quotes_fts.rowid ORDER BY RANDOM() LIMIT 1'''
+            out = dba.query_db(query)
+        else:
+            idq = re.match('id:', toks[1])
+            if idq:
+                idx = toks[1].split(':', 1)
+                if idx[1].isdigit():
+                    query = '''SELECT id, quote, added_by, quote_dt FROM quotes,quotes_fts WHERE quotes.id=quotes_fts.rowid AND id == ?'''
+                    pattern = idx[1]
+                else:
+                    query = None
+            else:
+                query = '''SELECT id, quote,added_by,quote_dt FROM quotes,quotes_fts WHERE quotes.id=quotes_fts.rowid AND quotes_fts MATCH ? ORDER BY RANDOM()'''
+                pattern = '%s' % ' ' . join(toks[1:])
+
+            if query:
+                out = dba.query_db(query, pattern)
+
+        #logging.debug(out)
+        if out and (len(out) >= 2):
+            quote_id = out[0] # id
+            quote_text = out[1] # quote
+
+        quote_time = ''
+        quote_user = ''
+        if out and (len(out) == 4):
+            quote_user = out[2] # user
+            quote_time = out[3] # time
+            quote_time = quote_time.replace('-', ':', 2)
+
+        if quote_text:
+            final_text = '[' + 'ID:' + str(quote_id) + ' by:' + quote_user + ' at:' + quote_time + '] ' + quote_text
+
+        return final_text
+
+
+    def addtodb(self, user, channel, msg):
+        ret = None
+        dba = db_api()
+        db = sqlite3.connect(DB_FILE)
+        dbc = db.cursor()
+        quote = ' ' . join(msg.split(' ')[1:])
+        query = '''INSERT INTO quotes_fts (QUOTE) VALUES (?)'''
+        dbc.execute(query,(quote,))
+        query = '''INSERT INTO quotes (QUOTE_DT, ADDED_BY, CHANNEL) VALUES (?, ?, ?)'''
+        dbc.execute(query, (time.strftime('%H-%M-%S %d-%m-%Y'), user, channel))
+        db.commit()
+        db.close()
+
+        #No intention of deleting, ever...
+        query = '''SELECT MAX(rowid) FROM quotes'''
+        max_q = dba.query_maxid(query)
+        ret = 'Quote ' + str(max_q) + ' added!'
+        return ret
+
+class db_api:
+
+    def __init__(self):
+        """
+        """
+
+
+    def init_db(self):
+        db = sqlite3.connect(DB_FILE)
+        dbc = db.cursor()
+        dbc.execute('''CREATE VIRTUAL TABLE IF NOT EXISTS QUOTES_FTS USING fts5(
+                                                    QUOTE
+                                                    )''')
+
+        dbc.execute('''CREATE TABLE IF NOT EXISTS QUOTES (ID INTEGER PRIMARY KEY,
+                                                    QUOTE_DT DATETIME,
+                                                    ADDED_BY TEXT,
+                                                    CHANNEL TEXT
+                                                    )''')
+
+        db.close()
+
+
+    def query_db(self, query, pattern = None):
+        db = sqlite3.connect(DB_FILE)
+        dbc = db.cursor()
+
+        if pattern:
+            dbc.execute(query, (pattern,))
+        else:
+            dbc.execute(query,)
+
+        out = dbc.fetchone()
+        db.close()
+        return out
+
+
+    def add_dbrow(self, query, quote, user = None, channel = None):
+        db = sqlite3.connect(DB_FILE)
+        dbc = db.cursor()
+        dbc.execute(query, (quote, time.strftime('%H-%M-%S %d-%m-%Y'), user, channel))
+        db.commit()
+        db.close()
+
+
+    def query_maxid(self, query):
+        db = sqlite3.connect(DB_FILE)
+        dbc = db.cursor()
+        dbc.execute(query,)
+        max_q = dbc.fetchone()
+        db.close()
+        return max_q[0]
 
 if __name__ == "__main__":
+    db_api().init_db()
     r = start_rtm()
     ws = websocket.WebSocketApp(r, on_message = on_message, on_error = on_error, on_close = on_close, on_open = on_open)
     ws.run_forever()
