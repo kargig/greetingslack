c800b07aeaa30bdd974216a524350014f8401d07
George Kargiotakis <kargig@void.gr>
2021-12-23 11:14:18 +0200

    Add the ability to store URLs posted in channel
    Saves URLs in a new table in the database
    Sends a message to the channel if the URL has been posted before
    
    Perform some python linting (flake8)

--- bot.py
+++ bot.py
@@ -5,48 +5,56 @@ import logging
 import websocket
 import json
 import requests
-import urllib
 import os
 import sys
 import sqlite3
 import time
 import re
 
-logging.basicConfig(filename='bot.log', level=logging.DEBUG,
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
 # Suppress InsecureRequestWarning
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-###VARIABLES THAT YOU NEED TO SET MANUALLY IF NOT ON HEROKU#####
+logging.basicConfig(filename='/backup/greetingslack/bot.log', level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(funcName)s() - %(levelname)s - %(message)s')
+
+
+# VARIABLES THAT YOU NEED TO SET MANUALLY IF NOT ON HEROKU
 # Handle each one seperately
 try:
-        TOKEN = os.environ['SLACK_TOKEN']
+    TOKEN = os.environ['SLACK_TOKEN']
 except:
-        TOKEN = 'Manually set the API Token if youre not running through heroku or have not set vars in ENV'
+    TOKEN = 'Manually set the API Token if youre not running through heroku or have not set vars in ENV'
 
 try:
-        UNFURL = os.environ['UNFURL_LINKS']
+    UNFURL = os.environ['UNFURL_LINKS']
 except:
-        UNFURL = 'FALSE'
+    UNFURL = 'FALSE'
 
 try:
-        DEBUG_CHANNEL_ID = os.environ['DEBUG_CHANNEL_ID']
+    DEBUG_CHANNEL_ID = os.environ['DEBUG_CHANNEL_ID']
 except:
-        DEBUG_CHANNEL_ID = 'Manually set the Channel if youre not running through heroku or have not set vars in ENV'
+    DEBUG_CHANNEL_ID = 'Manually set the Channel if youre not running through heroku or have not set vars in ENV'
 
 try:
-        DB_FILE = os.environ['DB_FILE']
+    DB_FILE = os.environ['DB_FILE']
 except:
-        DB_FILE = 'FALSE'
+    DB_FILE = 'FALSE'
 
 try:
-        WELCOME_FILE = os.environ['WELCOME_FILE']
+    WELCOME_FILE = os.environ['WELCOME_FILE']
 except:
-        WELCOME_FILE = '/path/to/WELCOME_MESSAGE.txt'
+    WELCOME_FILE = '/path/to/WELCOME_MESSAGE.txt'
 
 ###############################################################
+
+
+def FindURL(string):
+    # findall() has been used
+    # with valid conditions for urls in string
+    url = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', string)
+    return url
+
 
 def unescape(s):
     s = s.replace("&lt;", "<")
@@ -55,22 +63,26 @@ def unescape(s):
     s = s.replace("&amp;", "&")
     return s
 
+
 def welcome_message():
     try:
         message = os.environ['WELCOME-MESSAGE']
     except:
-        with codecs.open(WELCOME_FILE,'r',encoding='utf8') as f:
+        with codecs.open(WELCOME_FILE, 'r', encoding='utf8') as f:
             message = f.read()
     return message
 
+
 def is_team_join(msg):
     return msg['type'] == "team_join"
+
 
 def is_debug_channel_join(msg):
     if (msg['type'] == "member_joined_channel" and msg['channel'] == DEBUG_CHANNEL_ID):
         return True
     else:
         return False
+
 
 def welcome_me(msg):
     if msg['type'] == 'message' and 'text' in msg.keys():
@@ -80,6 +92,7 @@ def welcome_me(msg):
             return False
     else:
         return False
+
 
 def is_message(msg):
     if msg['type'] == 'message':
@@ -94,24 +107,30 @@ def show_quote(msg):
     else:
         return False
 
+
 def add_quote(msg):
     if msg['type'] == 'message' and msg['text'] == '!add':
         return True
     else:
         return False
 
+
 def parse_message(message):
-    m = json.loads(message)
-    #logging.debug(m)
+    try:
+        m = json.loads(message)
+    except:
+        logging.debug(message)
+        return
+    # logging.debug(m)
     if is_team_join(m) or is_debug_channel_join(m) or welcome_me(m):
         user_id = m["user"]["id"] if is_team_join(m) else m["user"]
-        getdata= {
+        getdata = {
                 'token': TOKEN,
                 'user': user_id
                 }
         userdata = requests.get("https://slack.com/api/im.open", params=getdata)
         userdata = userdata.json()
-        #logging.debug(userdata)
+        # logging.debug(userdata)
         dmchannel = userdata["channel"]["id"]
         data = {
                 'token': TOKEN,
@@ -121,74 +140,120 @@ def parse_message(message):
                 'as_user': 'true',
         }
         if (UNFURL.lower() == "false"):
-          data['unfurl_link']='false'
-        #logging.debug(data)
-        send_message = requests.post("https://slack.com/api/chat.postMessage", data=data)
-        #logging.debug(send_message)
-        logging.debug('HELLO SENT: ' + user_id)
+            data['unfurl_link'] = 'false'
+            # logging.debug(data)
+            send_message = requests.post("https://slack.com/api/chat.postMessage", data=data)
+            # logging.debug(send_message)
+            # logging.debug('HELLO SENT: ' + user_id)
     elif is_message(m) and 'text' in m.keys():
         ret = None
-        #logging.debug(m)
-        if m['text'][0] != '!':
+        if 'bot_id' in m.keys():
             return
-        try:
-            cmd, args = m['text'].split(' ', 1)
-        except ValueError:
-            cmd = m['text']
-            args = ''
-        udata= {
-                'token': TOKEN,
-                'user': m['user']
-                }
-        userdata = requests.get("https://slack.com/api/users.info", params=udata)
-        userdata = userdata.json()
-        logging.debug(userdata)
-        cdata= {
-                'token': TOKEN,
-                'channel': m['channel']
-                }
-        channeldata = requests.get("https://slack.com/api/channels.info", params=cdata)
-        channeldata = channeldata.json()
-        logging.debug(channeldata)
-        try:
-            channel_name = channeldata['channel']['name']
-        except:
-            channel_name = m['channel']
-        f_args = [userdata['user']['profile']['display_name'], channel_name, m['text']]
-        if cmd == '!quote':
-            ret = quote_api().get_quote(*f_args)
-        elif cmd == '!add':
-            ret = quote_api().addtodb(*f_args)
+        if m['text'][0] != '!':
+            # logging.debug(m)
+            # u'blocks': [{u'elements': [{u'elements': [{u'url': u'http://koko.org', u'text': u'koko.org', u'type': u'link'}]
+            if 'blocks' in m.keys():
+                if 'url' in m['blocks'][0]['elements'][0]['elements'][0].keys():
+                    urls = FindURL(m['blocks'][0]['elements'][0]['elements'][0]['url'])
+                    if len(urls) > 0:
+                        # logging.debug(m['blocks']['elements']['elements']['url'])
+                        logging.debug("Found URL: " + m['blocks'][0]['elements'][0]['elements'][0]['url'])
+                        displayname = get_display_name(m)
+                        channel_name = get_channel_name(m)
+                        # trim last character from url which is a '>' added by slack
+                        if urls[0][:-1] == '>':
+                            final_URL = urls[0][:-1]
+                        else:
+                            final_URL = urls[0]
+                        ret = quote_api().get_url(final_URL)
+                        if ret:
+                            ret = ret.replace('@', '')
+                            data = {
+                                    'token': TOKEN,
+                                    'channel': m['channel'],
+                                    'text': ret,
+                                    'parse': 'full',
+                                    'as_user': 'true',
+                            }
+                            send_message = requests.post("https://slack.com/api/chat.postMessage", data=data)
+                            # logging.debug(send_message)
+                            return
+                        f_args = [displayname, channel_name, final_URL]
+                        ret = quote_api().addurltodb(*f_args)
+                    return
+        elif m['text'][0] == '!':
+            try:
+                cmd, args = m['text'].split(' ', 1)
+            except ValueError:
+                cmd = m['text']
+                args = ''
+            displayname = get_display_name(m)
+            channel_name = get_channel_name(m)
+            f_args = [displayname, channel_name, m['text']]
+            if cmd == '!quote':
+                ret = quote_api().get_quote(*f_args)
+            elif cmd == '!add':
+                ret = quote_api().addtodb(*f_args)
 
-        if ret:
-            ret = ret.replace('@', '')
-            data = {
-                    'token': TOKEN,
-                    'channel': m['channel'],
-                    'text': ret,
-                    'parse': 'full',
-                    'as_user': 'true',
+            if ret:
+                ret = ret.replace('@', '')
+                data = {
+                        'token': TOKEN,
+                        'channel': m['channel'],
+                        'text': ret,
+                        'parse': 'full',
+                        'as_user': 'true',
+                }
+                send_message = requests.post("https://slack.com/api/chat.postMessage", data=data)
+                # logging.debug(send_message)
+
+
+def get_display_name(m):
+    # logging.debug("GET_DISPLAY_NAME")
+    udata = {
+            'token': TOKEN,
+            'user': m['user']
             }
-            send_message = requests.post("https://slack.com/api/chat.postMessage", data=data)
-            logging.debug(send_message)
+    userdata = requests.get("https://slack.com/api/users.info", params=udata)
+    userdata = userdata.json()
+    # logging.debug(userdata)
+    return(userdata['user']['profile']['display_name'])
 
-#Connects to Slacks and initiates socket handshake
+
+def get_channel_name(m):
+    # logging.debug("GET_CHANNEL_NAME")
+    cdata = {
+            'token': TOKEN,
+            'channel': m['channel']
+            }
+    channeldata = requests.get("https://slack.com/api/conversations.info", params=cdata)
+    channeldata = channeldata.json()
+    try:
+        channel_name = channeldata['channel']['name']
+    except:
+        channel_name = m['channel']
+    return(channel_name)
+
+
+# Connects to Slacks and initiates socket handshake
 def start_rtm():
     r = requests.get("https://slack.com/api/rtm.start?token="+TOKEN)
     r = r.json()
-    #logging.debug(r)
+    # logging.debug(r)
     r = r["url"]
     return r
+
 
 def on_message(ws, message):
     parse_message(message)
 
+
 def on_error(ws, error):
     try:
-		logger = logging.getLogger()
-		logger.error("SOME ERROR HAS HAPPENED: " + error)
+        logger = logging.getLogger()
+        logger.error("SOME ERROR HAS HAPPENED: " + error)
     except:
-        print 'Interrupted'
+        print('Interrupted')
         try:
             sys.exit(0)
         except SystemExit:
@@ -198,15 +263,16 @@ def on_error(ws, error):
 def on_close(ws):
     logging.info('Connection Closed')
 
+
 def on_open(ws):
     logging.info("Connection Started - Auto Greeting new joiners to the network")
+
 
 class quote_api:
 
     def __init__(self):
         """
         """
-
 
     def get_quote(self, user, channel, msg):
         toks = msg.split(' ')
@@ -233,16 +299,16 @@ class quote_api:
             if query:
                 out = dba.query_db(query, pattern)
 
-        #logging.debug(out)
+        # logging.debug(out)
         if out and (len(out) >= 2):
-            quote_id = out[0] # id
-            quote_text = out[1] # quote
+            quote_id = out[0]    # id
+            quote_text = out[1]  # quote
 
         quote_time = ''
         quote_user = ''
         if out and (len(out) == 4):
-            quote_user = out[2] # user
-            quote_time = out[3] # time
+            quote_user = out[2]  # user
+            quote_time = out[3]  # time
             quote_time = quote_time.replace('-', ':', 2)
 
         if quote_text:
@@ -251,6 +317,23 @@ class quote_api:
 
         return final_text
 
+    def get_url(self, url):
+        dba = db_api()
+        final_text = None
+        out = None
+        query = '''SELECT URL_DT,ADDED_BY,CHANNEL,URL FROM URLS WHERE URL == ?'''
+        pattern = url
+        if query:
+            out = dba.query_db(query, pattern)
+        if out:
+            url_dt = out[0]       # timestamp
+            url_user = out[1]     # username
+            url_channel = out[2]  # channel
+            url_dt = url_dt.replace('-', ':', 2)
+            final_text = '[' + 'URL: ' + str(url) + ' posted first on: #' + url_channel + ' by: ' + url_user + ' at: ' + url_dt + '] '
+            # logging.debug(final_text)
+            return final_text
+        return False
 
     def addtodb(self, user, channel, msg):
         ret = None
@@ -259,17 +342,34 @@ class quote_api:
         dbc = db.cursor()
         quote = ' ' . join(msg.split(' ')[1:])
         query = '''INSERT INTO quotes_fts (QUOTE) VALUES (?)'''
-        dbc.execute(query,(quote,))
+        dbc.execute(query, (quote,))
         query = '''INSERT INTO quotes (QUOTE_DT, ADDED_BY, CHANNEL) VALUES (?, ?, ?)'''
         dbc.execute(query, (time.strftime('%H-%M-%S %d-%m-%Y'), user, channel))
         db.commit()
         db.close()
 
-        #No intention of deleting, ever...
+        # No intention of deleting, ever...
         query = '''SELECT MAX(rowid) FROM quotes'''
         max_q = dba.query_maxid(query)
         ret = 'Quote ' + str(max_q) + ' added!'
         return ret
+
+    def addurltodb(self, user, channel, url):
+        ret = None
+        dba = db_api()
+        db = sqlite3.connect(DB_FILE)
+        dbc = db.cursor()
+        query = '''INSERT INTO URLS (URL_DT, ADDED_BY, CHANNEL, URL) VALUES (?,?,?,?)'''
+        dbc.execute(query, (time.strftime('%H-%M-%S %d-%m-%Y'), user, channel, url))
+        db.commit()
+        db.close()
+
+        # No intention of deleting, ever...
+        query = '''SELECT MAX(rowid) FROM URLS'''
+        max_q = dba.query_maxid(query)
+        ret = 'URL ' + str(max_q) + ' added!'
+        return ret
+
 
 class db_api:
 
@@ -290,10 +390,16 @@ class db_api:
                                                     CHANNEL TEXT
                                                     )''')
 
+        dbc.execute('''CREATE TABLE IF NOT EXISTS URLS (ID INTEGER PRIMARY KEY,
+                                                    URL_DT DATETIME,
+                                                    ADDED_BY TEXT,
+                                                    CHANNEL TEXT,
+                                                    URL TEXT
+                                                    )''')
+
         db.close()
 
-
-    def query_db(self, query, pattern = None):
+    def query_db(self, query, pattern=None):
         db = sqlite3.connect(DB_FILE)
         dbc = db.cursor()
 
@@ -306,14 +412,12 @@ class db_api:
         db.close()
         return out
 
-
-    def add_dbrow(self, query, quote, user = None, channel = None):
+    def add_dbrow(self, query, quote, user=None, channel=None):
         db = sqlite3.connect(DB_FILE)
         dbc = db.cursor()
         dbc.execute(query, (quote, time.strftime('%H-%M-%S %d-%m-%Y'), user, channel))
         db.commit()
         db.close()
-
 
     def query_maxid(self, query):
         db = sqlite3.connect(DB_FILE)
@@ -323,9 +427,9 @@ class db_api:
         db.close()
         return max_q[0]
 
+
 if __name__ == "__main__":
     db_api().init_db()
     r = start_rtm()
-    ws = websocket.WebSocketApp(r, on_message = on_message, on_error = on_error, on_close = on_close, on_open = on_open)
+    ws = websocket.WebSocketApp(r, on_message=on_message, on_error=on_error, on_close=on_close, on_open=on_open)
     ws.run_forever()
-
